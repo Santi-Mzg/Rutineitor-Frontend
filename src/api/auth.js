76fc72a9c54545accd8837458a3bc97c31ab16a8
Santi <santimaszong@gmail.com>
2024-10-23 12:20:54 -0300

    Updates

--- src/api/auth.js
+++
@@ -1,6 +0,0 @@
-import axios from './axios'
-
-export const registerRequest = (user) => axios.post(`/register`, user)
-export const loginRequest = (user) => axios.post(`/login`, user)
-export const logoutRequest = () => axios.post(`/logout`)
-export const verifyTokenRequest = () => axios.get('/verify')
--- src/api/workout.js
+++
@@ -1,7 +0,0 @@
-import axios from './axios'
-
-export const getWorkoutRequest = (date) => axios.get(`/workout/${date}`)
-
-export const createOrUpdateWorkoutRequest = (workout) => axios.post(`/workout`, workout)
-
-export const deleteWorkoutRequest = (date) => axios.delete(`/workout/${date}`)
